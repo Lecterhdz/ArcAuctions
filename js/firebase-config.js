e357ab864fbb6e558c47845800ccf037a4c61836
Lecterhdz <dialectycam@gmail.com>
2026-05-12 11:36:25 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,12 +4,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyC43NWWCailgWjJrtaOKG5og1w8q47eRkU",
+  authDomain: "arcauctions-69c8c.firebaseapp.com",
+  projectId: "arcauctions-69c8c",
+  storageBucket: "arcauctions-69c8c.firebasestorage.app",
+  messagingSenderId: "598785052209",
+  appId: "1:598785052209:web:1dc58922d99aef77c10f10"
 };
 
 const app = initializeApp(firebaseConfig);
